1d6f7081a04a18b7b1b1fa20ee5a6814b8721d7f
axiomproject <johncharlesvergara01@gmail.com>
2024-12-28 01:17:19 +0800

    community

--- src/pages/sub/Community.tsx
+++ src/pages/sub/Community.tsx
@@ -15,6 +15,13 @@ const Community = () => {
   return (
     <div className="home-container">
  <img src={bannerImage} alt="Banner" className="banner-image" />
+ <div className="introduction">
+ <h1 className="Testimonial">Our Community</h1>
+ <h4 className="h4-testimonial">The KM Foundation’s community initiatives aim to empower and support Payatas residents through programs in health, education, and livelihood, fostering resilience and growth within one of the most underserved areas.</h4>
+ </div>
+ <div className="home-container">
+
+      </div>
     </div>
   );
 };
