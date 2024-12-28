fb05584fec026ddd9947b5e47f8e06a6ea4cefa1
axiomproject <johncharlesvergara01@gmail.com>
2024-12-28 16:18:27 +0800

    life text and banner

--- src/pages/Life.tsx
+++ src/pages/Life.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-
+import lifeImage from "../img/life.png"
 
 
 
@@ -12,7 +12,8 @@ const Life = () => {
 
   return (
     <div className="home-container" >
-
+<img src={lifeImage} alt="Banner" className="life-image" />
+<p className="lifetext">Welcome to the heart of KM Foundation, where every individual – staff, sponsored students, and sponsors – plays a vital role in crafting a narrative of hope, growth, and lasting impact. Here’s a glimpse into the unique experiences that define Life with KM Foundation</p>
     </div>
   );
 };
