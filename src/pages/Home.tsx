42f4184494995580c5c6464bd2b5d9fd0240bf25
axiomproject <johncharlesvergara01@gmail.com>
2024-12-29 19:33:20 +0800

    fundraise home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,9 @@ import "../styles/Layout.css";
 import { Button, Typography } from "@mui/material";
 import bannerImage from "../img/map.png"
 import footerImage from "../img/footerImage.png"
+import happinessIcon from '../img/happiness.svg';
+import loveIcon from '../img/love.svg';
+import sociallyIcon from '../img/social.svg';
 
 const buttons = [
   {
@@ -16,7 +19,7 @@ const buttons = [
 const Home = () => {
 
   return (
-    <div className="home-container">
+    <div className="page-container">
       <div className="button-row">
         <div className="main-button">
           <Typography variant="h3">
@@ -27,9 +30,62 @@ const Home = () => {
           </Typography>
         </div>
       </div>
-     
+      <div className="fund-container">
+      <div className="fundraising-section">
+      <h2>
+        Fundraising with <span className="highlight">Kapatid kita, Mahal kita</span> only takes a few minutes
+      </h2>
+      <div className="cards-container">
+        <div className="cards">
+          <img src={happinessIcon} alt="Give Happiness Icon" className="icon" />
+          <h3>Give Happiness</h3>
+          <p>Giving happiness to others is one of the most fulfilling things you can do in life</p>
+        </div>
+        <div className="cards">
+          <img src={loveIcon} alt="Share Love Icon" className="icon" />
+          <h3>Share Love</h3>
+          <p>Giving happiness to others is one of the most fulfilling things you can do in life</p>
+        </div>
+        <div className="cards">
+          <img src={sociallyIcon} alt="Build Socially Icon" className="icon" />
+          <h3>Build Socially</h3>
+          <p>Giving happiness to others is one of the most fulfilling things you can do in life</p>
+        </div>
+      </div>
+    </div>
+      </div>
         <h2 className="map">Payatas Rift</h2>
       <img src={bannerImage} alt="Banner" className="banner-image" />
+    
+      <div className="page-container">
+    <div className='firstSection'>
+        <div className='Contact'>
+            <div className='ContactUs'>
+                <h1 className="contact">Contact Us</h1>
+                <p className="contactp">If you have further questions or need any assistance, please get in touch with our
+                team and we will gladly assist you.</p>
+                <p className="contactp">Kmkkpayatas@gmail.com</p>
+                <p className="contactp">321-221-221</p>
+                <a className="contactc" href="facebook.com" target="facebook.com">Contact Support</a>
+            </div>
+            <div className='inquiry'>
+              <div className="supp">
+                <h4 className="inq">Contact Support</h4>
+                <p className="inqp">Provides exceptional customer assistance through chat, resolving inquiries, and addressing concerns.</p>
+                </div>
+                <div className="feedback">
+                  <h4 className="inq">Feedback and Suggestions</h4>
+                  <p className="inqp">Collects, analyzes, and addresses user feedback and suggestions to improve products, services, and overall customer satisfaction effectively.</p>
+                </div>
+                <div className="inquiries">
+                  <h4 className="inq">Made Inquiries</h4>
+                  <p className="inqp">Handles and responds to inquiries efficiently, providing accurate information and solutions to ensure customer satisfaction and clarity.</p>
+                </div>
+        </div>
+        </div>
+
+    </div>
+      </div>
   <div className="footerimage">
       <img src={footerImage} alt="Banner" className="footer-image" />
       <div className="footertext">
@@ -40,6 +96,7 @@ const Home = () => {
       </div>
       </div>
     </div>
+    
   );
 };
 
---
+++ src/pages/sub/Contact.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>Contact
+    <div className='1st Section'>
+        <div className='Contact'>
+            <div className='ContactUs'>
+                <h1>Contact Us</h1>
+                <p>If you have further questions or need any assistance, please get in touch with our
+                team and we will gladly assist you.</p>
+                <p>Kmkkpayatas@gmail.com</p>
+                <p>321-221-221</p>
+            </div>
+            <div className='inquiry'>
+                <h1>Contact Support</h1>
+            </div>
+        </div>
+        <div className='FormContainer'>
+            <div className='Form'>
+                <h1 className='idk'>Get In Touch</h1>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Contact
